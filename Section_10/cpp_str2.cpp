3505d7b6d1f47e883d12d55ac799ff6e41710d76
Krishna <krishnamdev282@gmail.com>
2025-03-14 23:46:17 +0530

    strimg

--- Section_10/cpp_str2.cpp
+++ Section_10/cpp_str2.cpp
@@ -14,6 +14,35 @@ int main()
     string{s1, 0, 3};
     string(10, 'X');
 
-    cout << s0 << endl; // no garbage value
-    cout << s0.length() << endl;
+    // cout << s0 << endl; // no garbage value
+    // cout << s0.length() << endl;
+
+    // // comparison
+    // cout << boolalpha;
+    // cout << s1 << "==" << s5 << ":" << (s1 == s5) << endl;
+    // cout << s1 << "==" << s2 << ":" << (s1 == s2) << endl;
+    // cout << s1 << "!=" << s2 << ":" << (s1 != s2) << endl;
+    // cout << s1 << "<" << s5 << ":" << (s1 < s2) << endl;
+    // cout << s1 << "==" << "Apple" << ":" << (s1 == "Apple") << endl;
+
+    // // assignment
+
+    // s1 = "Mango";
+    // cout << "s1 is now : " << s1 << endl;
+    // s2 = s1;
+    // cout << "s2 is now : " << s2 << endl;
+
+    // s3 = "Krishna";
+    // cout << "s3 is now : " << s3 << endl;
+
+    // s3[0] = 'C';
+    // cout << "s3 first letter is now changed to 'C' : " << s3 << endl;
+
+    // s3.at(1) = 'i';
+    // cout << "s3 second letter is now changed to 'i' : " << s3 << endl;
+
+    // concatenation
+    s3 = "Watermelon";
+
+    s3 =
 }
